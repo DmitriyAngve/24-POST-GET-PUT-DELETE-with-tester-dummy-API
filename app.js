eb9fe860137f16c6a21b3ed4a7ba39a0bac7de37
Dmitriy Angve <kirirch129@gmail.com>
2023-02-01 15:53:13 +0300

    Add logic

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const btn = document.querySelector(".btn");
 const inpEle = document.querySelector("input");
 const output = document.querySelector(".output");
-const myForm = document.querySelector("form");
-
-inpEle.value = "Hello World";
-
+inpEle.value = 1;
+inpEle.classList.add("num");
+inpEle.setAttribute("type", "number");
+btn.textContent = "Select Post ID";
 btn.addEventListener("click", loadData);
+const baseURL = "https://jsonplaceholder.typicode.com/";
 
 function loadData(e) {
   console.log("ready");
+  const url = baseURL + "posts/" + inpEle.value;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      addtoPage(data);
+    });
+}
+
+function addtoPage(info) {
+  console.log(info);
+  const main = addEle(output, "div", "");
+  let html = `<h1>${info.title}</h1>`;
+  html += `<p>${info.body}</p>`;
+  html += `<small>ID: ${info.id} - UserID: ${info.userId}</small>`;
+  const item = addEle(main, "div", html);
+}
+
+function addEle(parent, t, html) {
+  const ele = document.createElement(t);
+  parent.prepend(ele);
+  ele.innerHTML = html;
+  return ele;
 }
